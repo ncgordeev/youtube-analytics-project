6930214369fcdda1fb4d5241260a34245f02781a
ncgordeev <ncgordeev@gmail.com>
2024-01-04 21:59:28 +0300

    add classmethod get_service

--- src/channel.py
+++ src/channel.py
@@ -3,8 +3,6 @@ from config import YT_API_KEY
 
 from googleapiclient.discovery import build
 
-youtube = build('youtube', 'v3', developerKey=YT_API_KEY)
-
 
 class Channel:
     """Класс для ютуб-канала"""
@@ -13,7 +11,15 @@ class Channel:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
 
+    @classmethod
+    def get_service(cls):
+        youtube_api_obj = build('youtube', 'v3', developerKey=YT_API_KEY)
+        return youtube_api_obj
+
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         current_channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         print(json.dumps(current_channel, indent=2, ensure_ascii=False))
+
+
+youtube = Channel.get_service()
